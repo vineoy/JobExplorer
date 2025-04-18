d53edcb873383167883ec79dcd91c9d8996dfe90
vineoy <113031024+vineoy@users.noreply.github.com>
2025-04-19 03:56:00 +0530

    Update api.ts

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://job-explorer-liard.vercel.app/',
+  baseURL: 'http://localhost:5000',
   headers: {
     'Content-Type': 'application/json'
   }
